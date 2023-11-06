f1e6d920a88f39c783d02d5cb9a21bfaeb354160
FixLuck <caube.vodoi5@gmail.com>
2023-11-06 15:59:26 +0700

    setup project

---
+++ src/main/java/com/ps34827/quanlynhansu/QuanLyNhanSu.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ps34827.quanlynhansu;
+
+/**
+ *
+ * @author caube
+ */
+public class QuanLyNhanSu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
