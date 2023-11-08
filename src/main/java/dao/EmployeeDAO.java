992ea635c493a666fdbd6f0395ff7965fbab46c7
FixLuck <caube.vodoi5@gmail.com>
2023-11-08 12:01:32 +0700

    add DAO

---
+++ src/main/java/dao/EmployeeDAO.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import entity.Employee;
+import java.util.List;
+
+/**
+ *
+ * @author Fixluck
+ */
+public class EmployeeDAO extends EntityDAO<Employee, String>{
+
+    @Override
+    public void insert(Employee entity) {
+        
+    }
+
+    @Override
+    public void update(Employee entity) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Employee> selectAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Employee selectByID(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Employee> selectBySql(String sql, Object... args) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+}
---
+++ src/main/java/dao/EntityDAO.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.util.List;
+
+/**
+ *
+ * @author Fixluck
+ * @param <E>: Entity
+ * @param <Key>: ID của các class
+ */
+abstract public class EntityDAO<E, Key> {
+
+    abstract public void insert(E entity);
+
+    abstract public void update(E entity);
+
+    abstract public void delete(Key id);
+
+    abstract public List<E> selectAll();
+
+    abstract public E selectByID(Key id);
+
+    abstract public List<E> selectBySql(String sql, Object... args);
+
+}
