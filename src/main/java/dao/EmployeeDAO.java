74fe19fc94172cc3bd31627db3afb4ed80e9668b
Anne672 <annedoan0589@gmail.com>
2023-11-15 18:01:22 +0700

    âcscasc

--- src/main/java/dao/EmployeeDAO.java
+++ src/main/java/dao/EmployeeDAO.java
@@ -129,12 +129,10 @@ public class EmployeeDAO extends EntityDAO<Employee, String> {
         return list;
     }
 
-//    public static void main(String[] args) {
-//        EmployeeDAO dao = new EmployeeDAO();
-//        List<Employee> list = dao.selectAll();
-//        for (Employee employee : list) {
-//            System.out.println(employee.getFirstName());
-//        }
-//    }
+    public static void main(String[] args) {
+        EmployeeDAO dao = new EmployeeDAO();
+        Employee e =dao.selectByID("E001");
+        System.out.println(e.getDepartment().getDepName());
+    }
 
 }
