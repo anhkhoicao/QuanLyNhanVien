ea93f14fddd8472186bc1e4e45f0979c5db25aba
FixLuck <caube.vodoi5@gmail.com>
2023-11-27 17:55:08 +0700

    fix time

--- src/main/java/UI/TimeSheetJDialog2.java
+++ src/main/java/UI/TimeSheetJDialog2.java
@@ -344,6 +344,8 @@ public class TimeSheetJDialog2 extends javax.swing.JDialog {
     private void btnCheckActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCheckActionPerformed
         // TODO add your handling code here:
         createEntity();
+        fillComboBoxYear();
+        fillAttendanceTable();
     }//GEN-LAST:event_btnCheckActionPerformed
 
     private void cboMonthSalaryActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cboMonthSalaryActionPerformed
@@ -507,7 +509,7 @@ public class TimeSheetJDialog2 extends javax.swing.JDialog {
                 model.addRow(rowData);
             }
         } catch (Exception e) {
-            MsgBox.alert(this, "Lỗi truy vấn dữ liệu");
+            System.out.println(e);
         }
     }
 
